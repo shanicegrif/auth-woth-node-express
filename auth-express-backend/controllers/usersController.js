9e5a6640913984b570b303f329a6b7a37a9c2f95
Shanice <shanicegriffin@pursuit.org>
2023-12-19 19:38:01 -0500

    finished back functionality

--- auth-express-backend/controllers/usersController.js
+++ auth-express-backend/controllers/usersController.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const bcrypt = require("bcryptjs");
-
+const bcrypt = require("bcryptjs");
+const { checkName } = require("../validations/checkUser.js")
 const {
   getOneUser,
   getOneUserByEmail,
@@ -10,14 +10,52 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const users = express.Router();
 
+users.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const oneUser = await getOneUser(userId);
+  if (oneUser) {
+    res.json(oneUser);
+  } else {
+    res.status(404).json({ error: "User Not Found" });
+  }
+});
+
 // LOGIN ROUTE
 users.post("/login", async (req, res) => {
-
+  try {
+    const { email, password } = req.body;
+    const user = await getOneUserByEmail(email);
+    if(user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        res.json({message: "Login successful", user});
+      } else {
+        res.status(401).json({ error: "Invalid credentials" });
+      }
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error logging in" });
+  }
 });
 
 // SIGN UP ROUTE
 users.post("/", async (req, res) => {
-  
+  try {
+    console.log("Request Body:", req.body)
+    const { password, ...userData } = req.body;
+    console.log("Processed User Data:", userData);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const createdUser = await createUser({
+      ...userData,
+      password: hashedPassword,
+    })
+    res.json(createdUser);
+  } catch (error) {
+    res.status(500).json({ error: "Error Creating User" });
+  }
 });
 
 
--- auth-express-backend/queries/users.js
+++ auth-express-backend/queries/users.js
@@ -5,13 +5,30 @@ const db = require("../db/dbConfig.js");
 
 
 const getOneUser = async (id) => {
-
+    try {
+        const oneUser = await db.one("SELECT * FROM users WHERE id=$1", id);
+        return oneUser;
+    } catch (error) {
+        console.error(error);
+    }
 };
 const getOneUserByEmail = async ({email}) => {
-
+    try {
+        const oneUser = await db.one("SELECT * FROM users WHERE email=$1", email)
+        return oneUser;
+    } catch (error) {
+        console.error(error);
+    }
 };
 const createUser = async (user) => {
-
+    try {
+        const {firstname, lastname, email, password} = user;
+        const createdUser = await db.one("INSERT INTO users (firstname, lastname, email, password) VALUES ($1, $2, $3, $4) RETURNING *", [firstname, lastname, email, password])
+        console.log(createdUser);
+        return createdUser;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 
